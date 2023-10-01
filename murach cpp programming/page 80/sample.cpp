ec21d006fb98a8967b6395a9f1ec562cbe855135
Ryan Chu <ryanchu2013@outlook.com>
2023-10-01 15:41:28 -0700

    calculates area based  on height and width

---
+++ murach cpp programming/page 80/sample.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+     double height;
+     double width;
+     double area;
+     cout<< "Rectangle Calculator\n\n";
+     cout << "enter height:";
+     cin >> height;             
+     cout << "enter width:";
+     cin >> width;
+     area = width * height;
+     cout<< "Area:"<< area << endl;
+}                                                                   
+                                                                                                                                         
