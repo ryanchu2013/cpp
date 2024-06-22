f3b066af2755e8c915c08c8fb1f493d51703304d
Ryan Chu <ryanchu2013@outlook.com>
2024-06-21 18:09:38 -0700

    calculates area of right triangles

---
+++ exercise/triangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+int main()
+{
+    cout << " welcome to the right triangle area calculator \n\n"; // tells user about use
+    double legnth; // data type
+    double hight; // data type
+    double area; // data type
+ cout << " enter legnth: "; // prompts user to write values for legnth and hight
+ cin >> legnth; // extracts info
+ cout << " enter hight: ";
+ cin >> hight; // extracts info
+     // calculations
+  area = legnth * hight / 2;  // calculate
+  cout << " your area is equal to: " << area << " \n\n" ; // telling user
+  cout << " bye skibdi slicer "; // skibidi
+}
