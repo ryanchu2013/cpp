d1a23e1ea513e71c206fda173348afd4503ce7b4
Ryan Chu <ryanchu2013@outlook.com>
2024-06-25 17:55:45 -0700

    Calculates area of rectangles

---
+++ murach cpp programming/assessment.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double height;
+    double width;
+    cout << " Welcome to the rectangle calculator \n\n";
+    cout << " Enter height \n\n ";
+    cin >> height;
+    cout << " Enter width \n\n";
+    cin >> width;
+    cout << " Your area is: " << height * width << "\n\n";
+    cout << " Bye! \n\n"; 
+
+}
