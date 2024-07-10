d5a54e3f9149fec9edbb4f32bc5d7b05583a2145
Ryan Chu <ryanchu2013@outlook.com>
2024-07-09 18:05:36 -0700

    goofy ahh quiz

---
+++ exercise/quiz.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double skibdi;
+    double fanumtax;
+    double mewing;
+    cout << " I ran out of ideas guys\n\n";
+    cout << " If skibdi means bad type one. if not, type 2 \n\n";
+    cin >> skibdi;
+    if ( skibdi == 1)
+    {
+        cout << " skibidi toilet will be yours yeah! you got it correct \n\n";
+    } else {
+        cout << " You live without the skibidi infection. You're wrong \n\n";
+    }
+    cout << " If fanum tax means the state takes your money, type 1. if not, type 2 \n\n";
+    cin >> fanumtax;
+    if (fanumtax == 2)
+    {
+        cout << " you like fanum taxing. You're correct \n\n";
+    }
+    else {
+        cout << " You're sigma. You're wrong\n\n";
+    }
+    cout << " If mewing means to make your jawline sharp, type one. If not, type two \n\n";
+    cin >> mewing;
+    if (mewing == 1)
+    {
+        cout << " You level 2000 looksmaxxer is correct \n\n";
+    }
+    else {
+        cout << " Okay your jawline is safe. You are fourtunately incorrect \n\n";
+    }
+    cout << " English or spanish \n\n";
+
+    
+       
+    
+}
